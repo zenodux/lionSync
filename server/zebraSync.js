61fac3b900f5040338168f751c2ea6b8b6c505d4
zeno dux <carlos@io.home>
2012-12-17 14:20:58 -0500

    Removed unnecessary stuff from zebraSync.js

--- server/zebraSync.js
+++ server/zebraSync.js
@@ -9,8 +9,17 @@ var persistenceSync = require('persistencejs/persistence.sync.server');
 // Database configuration
 //persistenceStore.config(persistence, 'localhost', 3306, 'synctest', 'test', 'test');
 
-var dbPath = __dirname + '/sqlite_zebraSync.db';
+var dbPath = './sqlite_zebraSync.db';
+console.log(dbPath);
 persistenceStore.config(persistence, dbPath);
+
+
+function generateVillageDummyData(session) {
+  var d = new Date();
+  var p = new village(session, {name: "ivy city", population: "10000", numLatrines: "20", dateMod: d.value });
+  session.add(p);
+  session.flush();
+}
 
 // Switch off query logging:
 //persistence.db.log = false;
@@ -21,39 +30,52 @@ function log(o) {
 
 persistenceSync.config(persistence);
 
-// Data model
-var Project = persistence.define('Project', {
-    name: "TEXT"
-  });
 
-var Task = persistence.define('Task', {
-    name: "TEXT",
-    done: "BOOL"
-  });
-
-var Tag = persistence.define('Tag', {
-    name: "TEXT"
-  });
-
-Task.hasMany('tags', Tag, 'tasks');
-Tag.hasMany('tasks', Task, 'tags');
-
-Project.hasMany('tasks', Task, 'project');
-
-Project.enableSync();
-Task.enableSync();
-Tag.enableSync();
 
 //Village
 
-var village = persistence.define('village', {
+var village;
+
+
+var session = persistenceStore.getSession();
+session.transaction(function(tx){
+  village = persistence.define('village', {
   name: "TEXT",
   population: "INT",
   numLatrines: "INT",
   datMod: "DATE"
+  },function(tx) { 
+    village.enableSync(function(tx){ 
+      session.schemaSync(tx, function() {
+        generateVillageDummyData(session);
+        session.flush(tx);
+      });
+    });
+  });
 });
+persistenceSync.config(session);
+session.close();
+
+
+// session.transaction(function(tx){
+//   village = persistence.define('village', {
+//   name: "TEXT",
+//   population: "INT",
+//   numLatrines: "INT",
+//   datMod: "DATE"
+//   });
+//   village.enableSync();
+//   session.flush(tx);
+//   session.schemaSync(tx, function() {
+//     generateVillageDummyData(session);
+    
+//     session.flush(tx);
+//   });
+// });
+
+
 //persistence.schemaSync();
-village.enableSync();
+//village.enableSync();
 //persistence.flush();
 
 //End Village
@@ -78,31 +100,11 @@ var app = express(
   }
 );
 
-function generateDummyData(session) {
-  var p = new Project(session, {name: "Main project"});
-  session.add(p);
-  for(var i = 0; i < 25; i++) {
-    var t = new Task(session, {name: "Task " + i, done: false});
-    p.tasks.add(t);
-  }
-}
-
-//Village
-
-function generateVillageDummyData(session) {
-  var d = new Date();
-  var p = new village(session, {name: "ivy city", population: "10000", numLatrines: "20", dateMod: d.value });
-  session.add(p);
-}
-
-//End Village
-
 
 // Actions
 app.get('/reset', function(req, res) {
   req.conn.reset(req.tx, function() {
       req.conn.schemaSync(req.tx, function() {
-          generateDummyData(req.conn);
           generateVillageDummyData(req.conn);
           req.conn.flush(req.tx, function() {
               res.send({status: "ok"});
@@ -110,42 +112,6 @@ app.get('/reset', function(req, res) {
         });
     });
 });
-
-app.get('/projectUpdates',  function(req, res) {
-    persistenceSync.pushUpdates(req.conn, req.tx, Project, req.query.since, function(updates) {
-        res.send(updates);
-      });
-});
-
-app.post('/projectUpdates',  function(req, res) {
-    persistenceSync.receiveUpdates(req.conn, req.tx, Project, req.body, function(result) {
-        res.send(result);
-      });
-  });
-
-app.get('/taskUpdates',  function(req, res) {
-    persistenceSync.pushUpdates(req.conn, req.tx, Task, req.query.since, function(updates) {
-        res.send(updates);
-      });
-});
-
-app.post('/taskUpdates',  function(req, res) {
-    persistenceSync.receiveUpdates(req.conn, req.tx, Task, req.body, function(result) {
-        res.send(result);
-      });
-  });
-
-app.get('/tagUpdates',  function(req, res) {
-    persistenceSync.pushUpdates(req.conn, req.tx, Tag, req.query.since, function(updates) {
-        res.send(updates);
-      });
-});
-
-app.post('/tagUpdates',  function(req, res) {
-    persistenceSync.receiveUpdates(req.conn, req.tx, Tag, req.body, function(result) {
-        res.send(result);
-      });
-  });
 
 //Village
 
@@ -160,7 +126,7 @@ app.post('/tagUpdates',  function(req, res) {
 app.get('/villageSync',  function(req, res) {
     console.log('villageSync GET from ' + req.conn);
     persistenceSync.pushUpdates(req.conn, req.tx, village, req.query.since, function(updates) {
-        //res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Origin", "*");
         res.send(updates);
     });
 });
@@ -168,7 +134,7 @@ app.get('/villageSync',  function(req, res) {
 app.post('/villageSync',  function(req, res) {
     console.log('villageSync POST from ' + req.conn);
     persistenceSync.receiveUpdates(persistenceStore.getSession(), req.tx, village, req.body, function(result) {
-        //res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Origin", "*");
         res.send(result);
     });
 });
@@ -199,4 +165,4 @@ app.get('/markAllUndone', function(req, res) {
 
 app.listen(1337);
 
-console.log('zebraSync Server running at http://127.0.0.1:1337/');
+console.log('zebraSync Basic Server running at http://127.0.0.1:1337/');
