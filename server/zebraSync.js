4ace7d6cb914917495f921faf9895b1d81f0229f
zeno dux <carlos@io.home>
2012-12-17 14:28:48 -0500

    zebraSync.js - syncing now works.
     connect.bodyParser() doesn't seem to be working - req.body was
    undefined. express.bodyParser() does the trick.  They are called
    differently, that may have something to do with the problem.  A lot of
    the demo code seems to use outdated syntax.  We may not need connect at
    all.  Along the way to finding out that connect.bodyParser() wasn't
    working, I added repl (read-evaluate-print loop, for you non-Lispers!)
    code that could help in future troubleshooting.  Variables can be
    attached to the repl for examination via
    .context.giveVariableAnyName=variableInNode
    
    (this was zebraSync_basic2.js - silly filesystem-based version
    control...)

--- server/zebraSync.js
+++ server/zebraSync.js
@@ -6,8 +6,10 @@ var persistence = require('persistencejs/persistence').persistence;
 var persistenceStore = require('persistencejs/persistence.store.sqlite3');
 var persistenceSync = require('persistencejs/persistence.sync.server');
 
+var repl = require("repl");
+
 // Database configuration
-//persistenceStore.config(persistence, 'localhost', 3306, 'synctest', 'test', 'test');
+
 
 var dbPath = './sqlite_zebraSync.db';
 console.log(dbPath);
@@ -16,7 +18,7 @@ persistenceStore.config(persistence, dbPath);
 
 function generateVillageDummyData(session) {
   var d = new Date();
-  var p = new village(session, {name: "ivy city", population: "10000", numLatrines: "20", dateMod: d.value });
+  var p = new village(session, {name: "ivy city", population: "10000", numLatrines: "20", _lastChange: d.value });
   session.add(p);
   session.flush();
 }
@@ -30,59 +32,34 @@ function log(o) {
 
 persistenceSync.config(persistence);
 
-
-
 //Village
 
 var village;
-
-
 var session = persistenceStore.getSession();
+
 session.transaction(function(tx){
   village = persistence.define('village', {
   name: "TEXT",
   population: "INT",
   numLatrines: "INT",
-  datMod: "DATE"
-  },function(tx) { 
-    village.enableSync(function(tx){ 
-      session.schemaSync(tx, function() {
-        generateVillageDummyData(session);
-        session.flush(tx);
-      });
-    });
+  _lastChange: "BIGINT"
+  });
+  session.schemaSync(tx, function(tx){ 
+    village.enableSync(tx, function(tx){
+      var p = new village(session, {name: "haswell city", population: "10000", numLatrines: "20"});
+      session.add(p);
+      persistence.flush(tx);
+    });  
   });
 });
-persistenceSync.config(session);
 session.close();
 
-
-// session.transaction(function(tx){
-//   village = persistence.define('village', {
-//   name: "TEXT",
-//   population: "INT",
-//   numLatrines: "INT",
-//   datMod: "DATE"
-//   });
-//   village.enableSync();
-//   session.flush(tx);
-//   session.schemaSync(tx, function() {
-//     generateVillageDummyData(session);
-    
-//     session.flush(tx);
-//   });
-// });
-
-
-//persistence.schemaSync();
-//village.enableSync();
-//persistence.flush();
 
 //End Village
 
 
 var app = express(
-  connect.logger(), 
+  //connect.logger(), 
   connect.bodyParser(), 
   //connect.staticCache('../browser'),
   function(req, res, next) {
@@ -99,70 +76,42 @@ var app = express(
       });
   }
 );
-
+app.use(express.bodyParser());
 
 // Actions
-app.get('/reset', function(req, res) {
-  req.conn.reset(req.tx, function() {
-      req.conn.schemaSync(req.tx, function() {
-          generateVillageDummyData(req.conn);
-          req.conn.flush(req.tx, function() {
-              res.send({status: "ok"});
-            });
+
+
+app.get('/villageSync',  function(req, res) {
+    var session = persistenceStore.getSession();
+    session.transaction(function(tx){
+        persistenceSync.pushUpdates(session, tx, village, req.query.since, function(updates) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.send(updates);
         });
     });
 });
 
-//Village
-
-// app.get('/villageSync',  function(req, res) {
-//     console.log('villageSync GET from ' + req.conn);
-//     persistenceSync.pushUpdates(req.conn, req.tx, village, req.query.since, function(updates) {
-//         //res.header("Access-Control-Allow-Origin", "*");
-//         res.send(updates);
-//     });
-// });
-
-app.get('/villageSync',  function(req, res) {
-    console.log('villageSync GET from ' + req.conn);
-    persistenceSync.pushUpdates(req.conn, req.tx, village, req.query.since, function(updates) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.send(updates);
-    });
-});
-
 app.post('/villageSync',  function(req, res) {
-    console.log('villageSync POST from ' + req.conn);
-    persistenceSync.receiveUpdates(persistenceStore.getSession(), req.tx, village, req.body, function(result) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.send(result);
+    var session = persistenceStore.getSession();
+    session.transaction(function(tx){
+        //console.log('updates (req.body): ' + req);
+        // repl.start({
+        //     prompt: "zebraSync > ",
+        //     input: process.stdin,
+        //     output: process.stdout
+        // }).context.req=req;
+        persistenceSync.receiveUpdates(session, tx, village, req.body, function(result) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.send(result);
+        });
     });
 });
 
-//End Village
 
-app.get('/markAllDone', function(req, res) {
-    Task.all(req.conn).list(req.tx, function(tasks) {
-        tasks.forEach(function(task) {
-            task.done = true;
-          });
-        req.conn.flush(req.tx, function() {
-            res.send({status: 'ok'});
-          });
-      });
-});
 
-app.get('/markAllUndone', function(req, res) {
-    Task.all(req.conn).list(req.tx, function(tasks) {
-        tasks.forEach(function(task) {
-            task.done = false;
-          });
-        req.conn.flush(req.tx, function() {
-            res.send({status: 'ok'});
-          });
-      });
-});
 
 app.listen(1337);
 
-console.log('zebraSync Basic Server running at http://127.0.0.1:1337/');
+console.log('zebraSync Basic 2 Server running at http://127.0.0.1:1337/');
+//console.log(app.routes);
+//console.log(app.routes.get[0].callbacks[0]);
