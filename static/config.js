552a529e83e1ffd66e4be0da466d995cb0df7f99
Owen Scott <owen.m.scott@gmail.com>
2013-02-27 11:35:32 +0300

    new demo UI
    new demo UI for testing with syncing to server. changes to config.js

--- static/config.js
+++ static/config.js
@@ -1,7 +1,47 @@
 
-var server = "http://opendev.the-carlos.net:1337"
+//var server = "http://opendev.the-carlos.net:1337"
 
 
+//=====db name here=====
+var dbName = 'testDb';
+
+
+//=====connect to DB=====
+
+try{
+  persistence.store.websql.config(persistence, dbName, 'Lion Local DB', 5 * 1024 * 1024);
+  console.log("Your browser supports WebSQL.")
+}
+catch(e){
+  persistence.store.memory.config(persistence);
+  console.log("Your browser does not supports WebSQL. We are using an in-memory DB and serialzing JSON to localStorage.");
+  try{
+    persistence.loadFromLocalStorage(function() {
+      console.log("Data loaded from localStorage");
+    });
+  }
+  catch(e){
+    console.log("Data *not* loaded from localStorage. There probably is no data. " + e)
+  }
+}
+
+
+
+//=====create new entity model for use in this example=====
+
+var Task = persistence.define('Task', {
+  name: "TEXT",
+  description: "TEXT",
+  done: "BOOL"
+});
+
+//sync schema
+persistence.schemaSync();
+
+
+//OLD config.js INCLUDED BELOW
+
+/*
 var village = persistence.define('village', {
   name: "TEXT",
   population: "INT",
@@ -17,4 +57,4 @@ village = persistence.define('village', {
   population: "INT",
   numLatrines: "INT",
   _lastChange: "BIGINT"
-  });
+  });*/
---
+++ static/main.js
@@ -0,0 +1,190 @@
+//=====READ ME=====
+//this script starts with a new object libary for creating auto-syncing input elements (e.g. text boxes, select boxes, etc.). you can ignore the first section, all you need to know is that it allows you to do things like "var t = new LionTextBox(lionField,lionFormat)" where lionField is an object containing the information needed to figure out which exact table, record, and field the text box should be linked to (e.g. Table: Village, Record: 6, Field: Name) and lionFormat is an object that passes along some details on how the input element should be formatted
+//lines 129-145 are where all the good stuff are. they create a dummy record (using a demo schema defined in config.js) and then link a new text box to one of the fields in that record. when you change the text box, the field should update automatically. run the program a bunch of times nad you'll generate some example data that you can play with.
+
+
+
+// to do
+	// - replace style with class
+	// - set up an object to contain and create a bunch of UI elements in a div
+
+//general inheritance method
+function clone(object) {
+	function ObjectConstructor() {
+		ObjectConstructor.prototype = object;
+	}
+	return new ObjectConstructor();
+}
+
+//OBJECT LIBRARY FOR CREATING PERSISTENCE USER INTERFACES
+
+//object class for passing identifying properties of individual fields to input objects
+function LionField (persistenceEntity, recordId, fieldName) {
+	this.persistenceEntity = persistenceEntity;
+	this.recordId = recordId;
+	this.fieldName = fieldName;
+	this.elementId = recordId + "-" + fieldName;
+}
+
+//object class for passing basic formatting to elements
+function LionFormat () {
+	this.font = 10;
+	this.width = 900;
+	this.height = 20;
+}
+
+//parent class for all input element classes
+function LionElement (lionfield, lionformat) {
+	this.element = function() {
+		//give element a unique id (concatenation of record id and field name)
+		e = this.rootElement.attr('id',lionfield.elementId)
+		//give element a change function that automatically updates the db whenever the element is changed
+		e.change(function() {
+			lionfield.persistenceEntity.load(lionfield.recordId,function(record) {
+				record[lionfield.fieldName] = document.getElementById(lionfield.elementId).value;
+				persistence.flush();
+			})
+		});
+		//give element a function that automatically populates it with a value based on its record value in the db 
+		//e.show(function() {
+		//	document.getElementById(lionfield.elementId).value = 7;
+		//});
+		e.show(function() {
+			lionfield.persistenceEntity.load(lionfield.recordId,function(record) {
+				if (!document.getElementById(lionfield.elementId).innerHTML) {document.getElementById(lionfield.elementId).innerHTML = record[lionfield.fieldName];}
+				document.getElementById(lionfield.elementId).value = record[lionfield.fieldName];
+			});
+		});
+		//apply formatting
+		e.width(lionformat.width);
+		//return element
+		return e;
+	}
+}
+
+//class for a text box input element (child of LionElement)
+function LionTextBox(lionfield,lionformat) {
+	LionElement.call(this,lionfield,lionformat);
+	this.rootElement = $('<input></input>').attr('type','text');
+}
+LionTextBox.prototype = clone(LionElement.prototype);
+LionTextBox.prototype.constructor = LionTextBox;
+
+//class for a select element (child of LionElement)
+function LionSelect(lionfield,lionformat,options) {
+	LionElement.call(this,lionfield,lionformat);
+	this.rootElement = $('<select></select>');
+	for (o in options) {
+		this.rootElement.append(
+		$('<option></option>').html(options[o].text).attr('value',options[o].value)
+		);
+	}
+	//this.rootElement.val(7);
+}
+LionSelect.prototype = clone(LionElement.prototype);
+LionSelect.prototype.constructor = LionSelect;
+
+
+//class for creating table cells (child of LionElement)
+function LionTableCell(lionfield,lionformat) {
+	LionElement.call(this,lionfield,lionformat);
+	this.rootElement = $('<td></td>');
+}
+LionTableCell.prototype = clone(LionElement.prototype);
+LionTableCell.prototype.constructor = LionTableCell;
+
+//class for creating a div containing numerous elements
+function LionDiv(persistenceEntity, recordId) {
+	this.rootElement = $('<div></div>');
+	this.element = function() {
+		return this.rootElement;
+	}
+}
+
+
+//END OF OBJECT LIBRARY
+
+//CUSTOM OBJECTS FOR THIS UI
+
+//class for creating a set of text boxes embedded in a table row
+function SanVillage(persistenceEntity,recordId) {
+	LionDiv.call(this,persistenceEntity,recordId);
+	var f = new LionField(persistenceEntity,recordId,'name');
+	var t = new LionTextBox(f,new LionFormat());
+	this.rootElement.append(t.element())//.append($('</br>'));
+} 
+SanVillage.prototype = clone(LionDiv.prototype);
+SanVillage.prototype.constructor = SanVillage.prototype;
+
+
+
+
+
+//EXAMPLE IMPLEMENTATION
+//
+
+
+
+
+//create a dummy record
+var test = new Task();
+test.name = "Carlos	";
+persistence.add(test);
+persistence.flush();
+
+//function creates 
+
+function initialize() {
+	
+	
+	Task.all().list(function(tasks){
+		tasks.forEach(function(task){
+			var v = new SanVillage(Task,task.id);
+			$('#test').append(v.element());
+		});
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//ignore everything below this for now
+	
+	
+	/*
+	var format = new LionFormat();
+	$('#test').append($('<table></table>').attr('id','testtable').append($('<tr></tr>').attr('id','testrow')));
+	
+	testCase = 'select';
+	
+	var selectOptions = [{text:'six',value:6},{text:'seven',value:7}];
+	
+	Task.all().list(function(tasks){
+		tasks.forEach(function(task){
+			var tempField = new LionField(Task,task.id,'name');
+			if (testCase == 'select') {
+				var temp = new LionSelect(tempField,format,selectOptions);
+				$('#test').append(temp.element());
+				$('#test').append('</br>');
+				
+			}
+			else if (testCase == 'text') {
+				var temp = new LionTextBox(tempField,format);
+				$('#test').append(temp.element());
+				$('#test').append('</br>');
+			}
+			else if (testCase == 'table') {
+				var temp = new LionTableCell(tempField,format);
+				$('#testrow').append(temp.element());
+			}				
+
+		});
+	});*/
+	
+}
+
