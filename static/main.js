1b8646689ca700dc7464d8b59a08a2bdb97d6fa0
Owen Scott <owen.m.scott@gmail.com>
2013-03-07 21:55:01 -0500

    fixed page handling

--- static/main.js
+++ static/main.js
@@ -7,9 +7,9 @@
 function showPage(page) {
 	//clear content div
 	emptyContentDiv();
-	$('#dataLink').attr('onclick','displayDataPage()');
-	$('#aboutLink').attr('onclick','displayAboutPage()');
-	$('#dashboardLink').attr('onclick','displayDashboardPage()');
+	//$('#dataLink').attr('onclick','displayDataPage()');
+	//$('#aboutLink').attr('onclick','displayAboutPage()');
+	//$('#dashboardLink').attr('onclick','displayDashboardPage()');
 	
 	if (page == 'data') {
 		//SHOW DATA PAGE
@@ -32,11 +32,11 @@ function showPage(page) {
 	}
 	else if (page=='syncFailure') {
 		$('#contentDiv').append('<p>LIONSync has failed to download content from the server.</p>');
-			$('a').attr('onclick','');
+			//$('a').attr('onclick','');
 	}
 	else if (page=='firstSync') {
 		$('#contentDiv').append('<div class=""><h4>Welcome to LIONSync</h4><p>Since this is your first time visiting, some content (~4mb) will need to be downloaded before you can begin. This may take a few minutes.</p></div>');
-		$('a').attr('onclick','');
+		//$('a').attr('onclick','');
 		//$('#contentDiv').append('<p>Downloading content...</p><img src="img/ajax-loader.gif"/>');
 	}
 }
