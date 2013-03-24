7f2dd0fc8405078f835790bfcd66739609c17431
zeno dux <carlos@io.home>
2013-03-24 09:26:28 -0400

    added finalist announcement and hashtags to about page

--- static/pages/about.js
+++ static/pages/about.js
@@ -13,8 +13,16 @@ function displayAboutPage() {
 	aboutDiv.append($("<p>This version is just a demo. It shows a simple 'data' page which you can use to add or edit villages (seriously, try it!). It then shows a simple dashboard where you can view summaries by district. The demonstration uses publicly available spatial data (GADM) for Malawi but all village-level data are fictional. This is just a demonstration to show the power of the idea and test the framework. We hope you like it! (For bonus points, try disconnecting your computer from the internet and adding a few villages offline.)</p>"));
 	aboutDiv.append($('<h3>LIONSync on Github</h3>'));
 	aboutDiv.append($("<p>Check out the repository <a href='https://github.com/zenodux/lionSync'>here</a></p>"));
+	aboutDiv.append($('<h3>LIONSync Youtube video</h3>'));
 	aboutDiv.append($('<iframe width="560" height="315" src="http://www.youtube.com/embed/VRU4IaLCVNw?rel=0" frameborder="0" allowfullscreen></iframe>'));
+	aboutDiv.append($('<h3>News</h3>'));
+	aboutDiv.append($('<ul>'));
+	aboutDiv.append($('<li>The World Bank <a href="http://www.sanitationhackathon.org/stories/world-bank-announces-sanitation-hackathon-app-challenge-finalists">announces</a> LIONSync is one of ten finalists in the Sanitation App Challenge</li> '));
+	aboutDiv.append($('</ul>'));
+	aboutDiv.append($('<h3>Follow us!</h3>'));
+	aboutDiv.append($('#lionsync #sanhack'));
 	aboutDiv.append($('<h3>Comments or Feedback</h3>'));
 	aboutDiv.append($("<p>Please send any comments or feedback to owen.m.scott [at] gmail.com or zenodux [at] gmail.com. Thanks!</p>"));
 	contentDiv.append($('<div></div>').addClass('row-fluid').append(aboutDiv));
 }
+
